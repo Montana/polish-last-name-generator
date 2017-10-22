ffe77276df510dd9b9d1405338e8702d0c8fd3bf
Montana Mendy <montanamendy@gmail.com>
2017-10-22 00:18:04 -0700

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,56 @@
+var nameW = ['Fampalski, Cranpalski, Pamppalski, Dabrowkski, Lewandowski, Kaminscki, Sampalski, Fanpalski, Crampalski,'];
+var nanmeM = ['Nampalski, Bampalski']
+
+function pickNameM(){
+    var userInput = inputForm.input.value;
+    var number = Number(userInput);
+
+    output.value = '';
+
+    for(let i = 0; i < number; i ++){
+        var pickedNameM = nameM[Math.floor(Math.random()*nameM.length)];
+        var pickedSurname = surname[Math.floor(Math.random()*surname.length)];
+        output.value += pickedNameM + ' ' + pickedSurname + '\n';
+    }
+}
+
+function pickNameW(){
+    var userInput = inputForm.input.value;
+    var number = Number(userInput);
+
+    output.value = '';
+
+    for(let i = 0; i < number; i ++){
+        var pickedNameW = nameW[Math.floor(Math.random()*nameW.length)];
+        var pickedSurname = surname[Math.floor(Math.random()*surname.length)];
+        var ski = new RegExp("ski$");
+        var cki = new RegExp("cki$");
+
+        if(ski.test(pickedSurname)){
+
+            var str2 = pickedSurname.replace(/ski$/,"ska");
+            output.value += pickedNameW + ' ' + str2 + '\n';
+        }
+        else if(cki.test(pickedSurname)){
+
+            var str2 = pickedSurname.replace(/cki$/,"cka");
+            output.value += pickedNameW + ' ' + str2 + '\n';
+        }
+        else{
+        output.value += pickedNameW + ' ' + pickedSurname + '\n';
+        }
+    }
+
+}
+
+function main(){
+    if (document.getElementById('women').checked){
+        pickNameW();
+    }
+    else if(document.getElementById('men').checked){
+        pickNameM();
+    }
+    else{
+        output.value = 'Błąd. Zaznacz jedną z płci.'
+    }
+}
